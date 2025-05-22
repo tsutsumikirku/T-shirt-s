d3a9397d037301b7e981fb65cb7761c1851864b3
junyoko1 <junyoko1@outlook.jp>
2025-05-22 18:32:53 +0900

    button

---
+++ Assets/Scripts/button/TranparentButton.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface TranparentBox
+{
+    // Start is called before the first frame update
+    public void Look(int looktime);
+}
