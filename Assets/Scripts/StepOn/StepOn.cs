f24bf8368832e301f58cee9a18f4a380b43a324a
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-05-22 18:22:53 +0900

    [add]enemyStepOn

--- Assets/Scripts/StepOn/StepOn.cs
+++ Assets/Scripts/StepOn/StepOn.cs
@@ -8,10 +8,12 @@ public class StepOn : MonoBehaviour
     /// このオブジェクトをプレイヤーが踏んだかどうか
     /// </summary>
     [HideInInspector] public bool playerStepOn;
+    [SerializeField] private float _jumpPower = 5f;
+    Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb= GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,8 @@ public class StepOn : MonoBehaviour
         {
             //ToDo: エネミーは踏まれたときに死亡する
             collision.gameObject.GetComponent<StepOnInterface>().OnStep();
+            _rb.AddForce(transform.up * _jumpPower, ForceMode2D.Impulse);
+            
         }
         else if (collision.gameObject.tag == "Box")
         {
