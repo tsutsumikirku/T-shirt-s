c5c5ac5ac7020dbf06cd710ea88ac5c5294c7ee8
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-05-22 15:01:37 +0900

    [add]StepOnのインターフェイスを実装した踏まれるクラスはこれを使用してください

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBase : MonoBehaviour
+public class EnemyBase : MonoBehaviour, StepOnInterface
 {
     [SerializeField] int _attackPower;
+    [SerializeField] private int score=10;
     public int AttackPower { get => _attackPower;}
+    public void OnStep()
+    {
+        ScoreManager.Instance.AddScore(score);
+        Destroy(this.gameObject);
+
+    }
 }
