4a02961db1f44ad8c7ee7bddbd26754b2945f39a
tsutsumikiriku <tsutsumikiriku@gmail.com>
2025-05-23 12:42:16 +0900

    [add]様々な修正

--- Assets/Kiya/BoxBreak.cs
+++ Assets/Kiya/BoxBreak.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class BoxBreak : MonoBehaviour, StepOnInterface
 {
     [SerializeField] int _boxLife;
+    [SerializeField]AudioClip _boxBreakSound;
 
     public void OnStep()
     {
         Console.WriteLine("Box broken");
+        if (_boxBreakSound)
+        {
+            SoundManager.Instance.PlaySE(_boxBreakSound);
+        }
         _boxLife--;
         if (_boxLife <= 0)
         {
--- Assets/Scripts/Player/DamageHit.cs
+++ Assets/Scripts/Player/DamageHit.cs
@@ -1,16 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class DamageHit : MonoBehaviour
 {
+    [SerializeField]LayerMask _enemyLayer;
+    [SerializeField] AudioClip _hitSE;
+    Rigidbody2D _rigidbody2D;
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
+            SoundManager.Instance.PlaySE(_hitSE);
             Debug.Log("Hit");
             int hitDamage = collision.gameObject.GetComponent<EnemyBase>().AttackPower;
             GameDataManager.Instance.Damage(hitDamage);
+            Hit();
         }
+    }
+    async UniTask Hit()
+    {
+        SpriteRenderer[] spriteRenderer = gameObject.GetComponentsInChildren<SpriteRenderer>();
+        _rigidbody2D.excludeLayers = _enemyLayer;
+        for (int i = 0; i < 5; i++)
+        {
+            Array.ForEach(spriteRenderer, x => x.color = new Color(1, 1, 1, 0f));
+            await UniTask.Delay(100);
+            Array.ForEach(spriteRenderer, x => x.color = new Color(1, 1, 1, 1));
+            await UniTask.Delay(100);
+        }
+        _rigidbody2D.excludeLayers = 0;
     }
 }
